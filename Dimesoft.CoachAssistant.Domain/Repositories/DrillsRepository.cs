89f2eb8b3fa83141f80bbd9eb3dd0bdf33dcc391
Derik Whittaker <Derik@graudo.com>
2012-08-19 09:04:40 -0400

    Tile Icon Change

--- Dimesoft.CoachAssistant.Domain/Repositories/DrillsRepository.cs
+++ Dimesoft.CoachAssistant.Domain/Repositories/DrillsRepository.cs
@@ -22,25 +22,28 @@ namespace Dimesoft.CoachAssistant.Domain.Repositories
 
             if (!results.Any())
             {
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 1, SportTypeId = (int)SportType.Soccer, Name = "1 on 1 Passing" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 2, SportTypeId = (int)SportType.Soccer, Name = "3 Person Passing" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 3, SportTypeId = (int)SportType.Soccer, Name = "4 Corner Passing" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 4, SportTypeId = (int)SportType.Soccer, Name = "Cone Dribbling" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 5, SportTypeId = (int)SportType.Soccer, Name = "Corner Kicks" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 6, SportTypeId = (int)SportType.Soccer, Name = "Throw Ins" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "1 on 1 Passing" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "3 Person Passing" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "4 Corner Passing" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Cone Dribbling" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Corner Kicks" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Throw Ins" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Goal Kicks" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Player Positioning" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { SportTypeId = (int)SportType.Soccer, Name = "Two-Foot Dribbling" });
 
 
 
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 10, SportTypeId = (int)SportType.Baseball, Name = "Drill 10" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 11, SportTypeId = (int)SportType.Baseball, Name = "Drill 11" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 12, SportTypeId = (int)SportType.Baseball, Name = "Drill 12" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 13, SportTypeId = (int)SportType.Baseball, Name = "Drill 13" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 14, SportTypeId = (int)SportType.Baseball, Name = "Drill 14" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 15, SportTypeId = (int)SportType.Baseball, Name = "Drill 15" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 16, SportTypeId = (int)SportType.Baseball, Name = "Drill 16" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 17, SportTypeId = (int)SportType.Baseball, Name = "Drill 17" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 18, SportTypeId = (int)SportType.Baseball, Name = "Drill 18" });
-                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 19, SportTypeId = (int)SportType.Baseball, Name = "Drill 19" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 20, SportTypeId = (int)SportType.Baseball, Name = "Drill 10" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 21, SportTypeId = (int)SportType.Baseball, Name = "Drill 11" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 22, SportTypeId = (int)SportType.Baseball, Name = "Drill 12" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 23, SportTypeId = (int)SportType.Baseball, Name = "Drill 13" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 24, SportTypeId = (int)SportType.Baseball, Name = "Drill 14" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 25, SportTypeId = (int)SportType.Baseball, Name = "Drill 15" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 26, SportTypeId = (int)SportType.Baseball, Name = "Drill 16" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 27, SportTypeId = (int)SportType.Baseball, Name = "Drill 17" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 28, SportTypeId = (int)SportType.Baseball, Name = "Drill 18" });
+                DB.Database.Save<PracticeDrillDto>(new PracticeDrillDto { Id = 29, SportTypeId = (int)SportType.Baseball, Name = "Drill 19" });
 
                 DB.Database.Flush();
             }
